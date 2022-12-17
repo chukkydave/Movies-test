4cd8918caf9256ec09474dd1bfe173c8dfd92c0a
Chuks <chukkydave@gmail.com>
2022-12-17 19:26:15 +0100

    added https

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 
   const searchMovie = async val => {
 
-    const response = await axios.get('http://www.omdbapi.com/', {
+    const response = await axios.get('https://www.omdbapi.com/', {
       params: {
         apikey: '56b94088',
         s: val,
